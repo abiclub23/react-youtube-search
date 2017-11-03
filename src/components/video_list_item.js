542c6de41c935e621ca7bc244e816f42745f848e
Abhi Tondepu <abhinash.tondepu@birdeye.com>
2017-11-02 22:09:35 -0700

    init

---
+++ src/components/video_list_item.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import VideoListItem from './video_list_item'
+//
+
+
+const VideoListItem = ({video, onVideoSelect}) => {
+	//const video = props.video;
+	const imgUrl = video.snippet.thumbnails.default.url;
+	return (
+		<li className="list-group-item" onClick= {() => onVideoSelect(video)}>
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imgUrl}/>
+				</div>
+				<div className="media-body">
+					<div className="media-heading">{video.snippet.title} </div>
+				</div>
+			</div>
+
+		</li>
+	);
+};
+
+
+export default VideoListItem;
